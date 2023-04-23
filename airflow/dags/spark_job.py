e9588ec2b6fb47bb364674001795a26281ca6c30
AliaHa3 <eng.alia.hamwi@gmail.com>
2023-04-23 11:29:13 +0300

    daily dag

--- airflow/dags/spark_job.py
+++ airflow/dags/spark_job.py
@@ -2,6 +2,20 @@ import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql import types
 from pyspark.sql import functions as F
+import argparse
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--input_file', required=True,default="")
+parser.add_argument('--output_file', required=True,default="")
+parser.add_argument('--output_table', required=True,default="")
+
+args = parser.parse_args()
+
+input_file = args.input_file
+output_file = args.output_file
+output_table = args.output_table
 
 spark = SparkSession.builder.master("local[*]").appName("test").getOrCreate()
 print(spark.version)
