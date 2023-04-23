d401c7eb5bce67c5b146064b7b1f5683d27f6f63
AliaHa3 <eng.alia.hamwi@gmail.com>
2023-04-23 13:08:28 +0300

    daily dag

--- airflow/dags/daily_DAG.py
+++ airflow/dags/daily_DAG.py
@@ -17,6 +17,88 @@ external_table_name = 'daily_staging'
 # dag = DAG('hourly_DAG', description='Hourly DAG', schedule_interval='5 * * * *',
 #   start_date=datetime(2023, 4, 22), catchup=False, max_active_runs=1, user_defined_macros=MACRO_VARS)
 
+def extract_data_to_local(ti,url, file_name, data_folder_path="."):
+    ti.xcom_push(key="general", value={"local_file_path":local_file_path,
+                                                     "file_name":file_name})
+    
+    df = pd.read_csv(url)
+    print(df.head())
+
+    # local_file_path = f"{data_folder_path}/{file_name}.csv.gz"
+
+    # df.to_parquet(path,index=False, compression="gzip")
+    df.to_csv(local_file_path, index=False, compression="gzip")
+    
+    return local_file_path
+
+
+def upload_to_bucket(ti,blob_name, bucket_name=GCP_GCS_BUCKET):
+    local_file_path = ti.xcom_pull(key="general", task_ids="extract_data_to_local")['local_file_path']
+
+    """ Upload data to a bucket"""
+    storage_client = storage.Client.from_service_account_json(
+        SERVICE_ACCOUNT_JSON_PATH)
+
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_filename(local_file_path)
+
+    # returns a public url
+    return blob.public_url
+
+
+def execute_query(query_str):
+    """ create table in BigQuery"""
+    bigquery_client = bigquery.Client.from_service_account_json(
+        SERVICE_ACCOUNT_JSON_PATH)
+    query_job = bigquery_client.query(query_str)
+
+    results = query_job.result()  # Waits for job to complete
+    print(results)
+    return results
+
+
+def check_table_exists(table_name):
+    """ check if table exists in BigQuery"""
+    try:
+        bigquery_client = bigquery.Client.from_service_account_json(
+            SERVICE_ACCOUNT_JSON_PATH)
+        result = bigquery_client.get_table(table_name)
+    except NotFound:
+        print(f" Table {table_name} is not found")
+        result = None
+    return result
+
+
+def create_external_table(ti,table_name, file_name="*"):
+    if file_name != "*":
+        file_name = ti.xcom_pull(key="general", task_ids="extract_data_to_local")['file_name']
+
+    is_exist = check_table_exists(
+        f'{GCP_PROJECT_ID}.{BQ_DATASET_STAGING}.{table_name}')
+    if is_exist is None:
+        query_str = f"""
+        CREATE OR REPLACE EXTERNAL TABLE `{GCP_PROJECT_ID}.{BQ_DATASET_STAGING}.{table_name}`
+        OPTIONS (
+        format = 'CSV',
+        uris = ['gs://{GCP_GCS_BUCKET}/earthquakes/{file_name}.csv.gz']
+        )
+        """
+        result = execute_query(query_str)
+    return result
+
+
+def print_hello():
+    print(file_name)
+    print(dataset_url)
+    print(local_file_path)
+    print(GCP_PROJECT_ID)
+    print(GCP_GCS_BUCKET)
+    print(BQ_DATASET_STAGING)
+    print(EXECUTION_DATETIME_STR)
+    return 'Hello world from first Airflow DAG!'
+
+
 with DAG('hourly_DAG', description='Hourly DAG', schedule_interval='5 * * * *',
          start_date=datetime(2023, 4, 22), catchup=False, max_active_runs=1) as dag:
 
@@ -54,74 +136,6 @@ with DAG('hourly_DAG', description='Hourly DAG', schedule_interval='5 * * * *',
     #               "EXECUTION_DATETIME_STR": EXECUTION_DATETIME_STR
     #               }
 
-    def extract_data_to_local(url, file_name, data_folder_path="."):
-        df = pd.read_csv(url)
-        print(df.head())
-
-        # local_file_path = f"{data_folder_path}/{file_name}.csv.gz"
-
-        # df.to_parquet(path,index=False, compression="gzip")
-        df.to_csv(local_file_path, index=False, compression="gzip")
-
-        return local_file_path
-
-    def upload_to_bucket(blob_name, path_to_file, bucket_name=GCP_GCS_BUCKET):
-        """ Upload data to a bucket"""
-        storage_client = storage.Client.from_service_account_json(
-            SERVICE_ACCOUNT_JSON_PATH)
-
-        bucket = storage_client.get_bucket(bucket_name)
-        blob = bucket.blob(blob_name)
-        blob.upload_from_filename(path_to_file)
-
-        # returns a public url
-        return blob.public_url
-
-    def execute_query(query_str):
-        """ create table in BigQuery"""
-        bigquery_client = bigquery.Client.from_service_account_json(
-            SERVICE_ACCOUNT_JSON_PATH)
-        query_job = bigquery_client.query(query_str)
-
-        results = query_job.result()  # Waits for job to complete
-        print(results)
-        return results
-
-    def check_table_exists(table_name):
-        """ check if table exists in BigQuery"""
-        try:
-            bigquery_client = bigquery.Client.from_service_account_json(
-                SERVICE_ACCOUNT_JSON_PATH)
-            result = bigquery_client.get_table(table_name)
-        except NotFound:
-            print(f" Table {table_name} is not found")
-            result = None
-        return result
-
-    def create_external_table(table_name, file_name="*"):
-        is_exist = check_table_exists(
-            f'{GCP_PROJECT_ID}.{BQ_DATASET_STAGING}.{table_name}')
-        if is_exist is None:
-            query_str = f"""
-			CREATE OR REPLACE EXTERNAL TABLE `{GCP_PROJECT_ID}.{BQ_DATASET_STAGING}.{table_name}`
-			OPTIONS (
-			format = 'CSV',
-			uris = ['gs://{GCP_GCS_BUCKET}/earthquakes/{file_name}.csv.gz']
-			)
-			"""
-            result = execute_query(query_str)
-        return result
-
-    def print_hello():
-        print(file_name)
-        print(dataset_url)
-        print(local_file_path)
-        print(GCP_PROJECT_ID)
-        print(GCP_GCS_BUCKET)
-        print(BQ_DATASET_STAGING)
-        print(EXECUTION_DATETIME_STR)
-        return 'Hello world from first Airflow DAG!'
-
     hello_operator = PythonOperator(
         task_id='hello_task', python_callable=print_hello, dag=dag)
 
@@ -138,8 +152,7 @@ with DAG('hourly_DAG', description='Hourly DAG', schedule_interval='5 * * * *',
         task_id=f"local_to_gcs_task",
         python_callable=upload_to_bucket,
         op_kwargs={
-            "blob_name": f"earthquakes/{file_name}.csv.gz",
-            "path_to_file": local_file_path
+            "blob_name": f"earthquakes/{file_name}.csv.gz"
         }
     )
 
@@ -153,7 +166,7 @@ with DAG('hourly_DAG', description='Hourly DAG', schedule_interval='5 * * * *',
         python_callable=create_external_table,
         op_kwargs={
             "table_name": f"{external_table_name}",
-            "file_name": file_name
+            "file_name": "file_name"
         }
     )
 
