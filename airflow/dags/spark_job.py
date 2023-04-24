e61af97c887f1a25b48700f7a1913441d600949f
AliaHa3 <eng.alia.hamwi@gmail.com>
2023-04-24 13:15:13 +0300

    daily dag

--- airflow/dags/spark_job.py
+++ airflow/dags/spark_job.py
@@ -3,6 +3,9 @@ from pyspark.sql import SparkSession
 from pyspark.sql import types
 from pyspark.sql import functions as F
 import argparse
+import os
+
+SERVICE_ACCOUNT_JSON_PATH = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
 
 
 parser = argparse.ArgumentParser()
@@ -19,3 +22,9 @@ output_table = args.output_table
 
 spark = SparkSession.builder.master("local[*]").appName("test").getOrCreate()
 print(spark.version)
+
+spark._jsc.hadoopConfiguration().set("google.cloud.auth.service.account.json.keyfile","SERVICE_ACCOUNT_JSON_PATH")
+
+path = "gs://earthquakes_data_lake_dezoomcamp-375819/earthquakes/data_20230424T090501_20230424T100501_20230424100501.csv.gz"
+df=spark.read.csv(path, header=True)
+df.show()
