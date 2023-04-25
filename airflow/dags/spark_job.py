bbe8d27b4a0d3381a37234e265dc6b41c7610d6e
AliaHa3 <eng.alia.hamwi@gmail.com>
2023-04-25 18:15:59 +0300

    spark

--- airflow/dags/spark_job.py
+++ airflow/dags/spark_job.py
@@ -36,6 +36,7 @@ def country_enrichment(row):
     row['country_code'] = country_code
     row['zipcode'] = zipcode
 
+    print(row)
     return row
 
 
@@ -77,11 +78,11 @@ path = "gs://earthquakes_data_lake_dezoomcamp-375819/earthquakes/data_20230424T0
 df=spark.read.csv(path, header=True)
 df.show()
 
-# new_df = df.apply(country_enrichment, axis=1)
-# new_df.show()
+new_df = df.rdd.map(lambda row: country_enrichment(row))
+new_df.show()
 
-df.write.format('bigquery') \
-    .option('table', 'earthquake_prod.tmptable2') \
-    .mode('append') \
-    .save()
+# df.write.format('bigquery') \
+#     .option('table', 'earthquake_prod.tmptable2') \
+#     .mode('append') \
+#     .save()
 
