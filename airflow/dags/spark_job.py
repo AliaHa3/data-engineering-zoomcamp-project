d97aac1ac05d74c9e005cc4f631eb8d998fad3b3
AliaHa3 <eng.alia.hamwi@gmail.com>
2023-04-29 14:12:38 +0300

    spark

--- airflow/dags/spark_job.py
+++ airflow/dags/spark_job.py
@@ -83,6 +83,19 @@ enrich_schema = types.StructType(
     ]
 )
 
+dwh_schema = types.StructType(
+    [
+        types.StructField("_year", types.FloatType(), True),
+        types.StructField("_month", types.FloatType(), True),
+        types.StructField("_day", types.FloatType(), True),
+        types.StructField("city", types.StringType(), True),
+        types.StructField("earthquakes_total_count", types.FloatType(), True),
+        types.StructField("max_depth", types.FloatType(), True),
+        types.StructField("max_mag", types.FloatType(), True),
+        types.StructField("avg_depth", types.FloatType(), True),
+        types.StructField("avg_mag", types.FloatType(), True)
+    ]
+)
 
 def country_enrichment(row):
     geolocator = Nominatim(user_agent="geoapiEnrichment")
@@ -167,26 +180,26 @@ enrich_df = spark.createDataFrame(enrich_rdd, schema=enrich_schema)
 enrich_df.printSchema()
 enrich_df.show()
 
-enrich_df.registerTempTable("enrich_full_data")
+# enrich_df.createOrReplaceTempView("enrich_full_data")
 
 
-earthquake_dwh_df = spark.sql(
-"""
-select 
-date_trunc(time, year) as _year, 
-date_trunc(time, month) as _month, 
-date_trunc(time, day) as _day,
-city,
-count(*) earthquakes_total_count, 
-max(depth) max_depth,
-max(mag) max_mag,
-avg(depth) avg_depth,
-avg(mag) avg_mag,
-from enrich_full_data
-group by 1,2,3,4;
-"""
-)
-earthquake_dwh_df.show()
+# earthquake_dwh_df = spark.sql(
+# """
+# select 
+# date_trunc(time, year) as _year, 
+# date_trunc(time, month) as _month, 
+# date_trunc(time, day) as _day,
+# city,
+# count(*) earthquakes_total_count, 
+# max(depth) max_depth,
+# max(mag) max_mag,
+# avg(depth) avg_depth,
+# avg(mag) avg_mag,
+# from enrich_full_data
+# group by 1,2,3,4;
+# """
+# )
+# earthquake_dwh_df.show()
 
 enrich_df.write.format('bigquery') \
     .option('table', f"{BQ_DATASET_PROD}.full_data2") \
@@ -197,8 +210,8 @@ enrich_df.write.format('bigquery') \
     .save()
 
 
-earthquake_dwh_df.write.format('bigquery') \
-    .option('table', f"{BQ_DATASET_PROD}.earthquake_dwh") \
-    .option("clusteredFields", "city") \
-    .mode('overwrite') \
-    .save()
+# earthquake_dwh_df.write.format('bigquery') \
+#     .option('table', f"{BQ_DATASET_PROD}.earthquake_dwh") \
+#     .option("clusteredFields", "city") \
+#     .mode('overwrite') \
+#     .save()
