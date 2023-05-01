6b74368762615e8e09ddcc811a033ba792ea0a89
AliaHa3 <eng.alia.hamwi@gmail.com>
2023-05-01 20:27:34 +0300

    setups

--- scripts/airflow_startup.sh
+++ scripts/airflow_startup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 pwd
-sudo chmod -R 777 dbt
+
 cd airflow
 docker-compose build
 docker-compose up airflow-init
 docker-compose up -d
 echo "Run 'docker-compose logs --follow' to see the logs."
+
