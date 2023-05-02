4ee3f4318fc51c97c7614537dfd2a74abfcf8054
AliaHa3 <eng.alia.hamwi@gmail.com>
2023-05-02 17:21:36 +0300

    setup

--- scripts/setup_config.sh
+++ scripts/setup_config.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-set -a
-GCP_PROJECT_ID=dezoomcamp-375819
-GCP_GCS_BUCKET=earthquakes_data_lake_dezoomcamp-375819
-BIGQUERY_DATASET=earthquake_prod
-TMP_BUCKET=dtc_data_lake_dezoomcamp-375819
-set +a
+export GCP_PROJECT_ID=dezoomcamp-375819
+export GCP_GCS_BUCKET=earthquakes_data_lake_dezoomcamp-375819
+export BIGQUERY_DATASET=earthquake_prod
+export TMP_BUCKET=dtc_data_lake_dezoomcamp-375819
